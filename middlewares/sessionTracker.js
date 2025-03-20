34b027eb131a7d1cc82c80ee6e0874cf6453c330
PRIYAM <you@example.com>
2025-03-20 19:14:10 +0530

    debugged claim coupon controller

--- middlewares/sessionTracker.js
+++ middlewares/sessionTracker.js
@@ -1,12 +1,13 @@
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 const sessionTracker = (req, res, next) => {
   let sessionId = req.cookies.sessionId;
+  console.log(sessionId)
   if (!sessionId) {
     sessionId = uuidv4();
   }
 
-  res.cookie("session", sessionId, {
+  res.cookie("sessionId", sessionId, {
     httpOnly: true,
     maxAge: 30 * 24 * 60 * 60 * 1000,
     sameSite: "strict",
