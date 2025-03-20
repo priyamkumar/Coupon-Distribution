c3eb30b5e0d65c75c9523db9078afd94d37eafea
PRIYAM <you@example.com>
2025-03-20 23:30:20 +0530

    changed session tracker for testing

--- middlewares/sessionTracker.js
+++ middlewares/sessionTracker.js
@@ -9,7 +9,7 @@ const sessionTracker = (req, res, next) => {
       httpOnly: true,
       maxAge: 30 * 24 * 60 * 60 * 1000,
       sameSite: 'none',
-      secure: false
+      secure: true
     });
   }
   req.sessionId = sessionId;
