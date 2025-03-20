82eda9d950c0321c4f3964fd8e10cafafc472950
PRIYAM <you@example.com>
2025-03-20 22:24:15 +0530

    sent coupon id to frontend

--- controllers/couponController.js
+++ controllers/couponController.js
@@ -15,6 +15,7 @@ const getCoupon = async (req, res) => {
     }
     return res.status(200).json({
       success: true,
+      couponId: availableCoupon._id,
       code: availableCoupon.code,
       info: availableCoupon.description,
     });
--- middlewares/sessionTracker.js
+++ middlewares/sessionTracker.js
@@ -2,18 +2,17 @@ const { v4: uuidv4 } = require("uuid");
 
 const sessionTracker = (req, res, next) => {
   let sessionId = req.cookies.sessionId;
-  console.log(sessionId)
   if (!sessionId) {
     sessionId = uuidv4();
-  }
 
-  res.cookie("sessionId", sessionId, {
-    httpOnly: true,
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    sameSite: "strict",
-  });
+    res.cookie("sessionId", sessionId, {
+      httpOnly: true,
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      sameSite: "none",
+      secure: true,
+    });
+  }
   req.sessionId = sessionId;
   next();
 };
-
 module.exports = { sessionTracker };
